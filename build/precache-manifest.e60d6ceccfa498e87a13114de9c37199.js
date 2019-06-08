7c75746457c4f97d667606c5c942a10af45723a1
Angel Arriaga <AngelArriaga@users.noreply.github.com>
2019-06-07 20:52:36 -0500

    updated build

---
+++ build/precache-manifest.e60d6ceccfa498e87a13114de9c37199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81397745e291bbf1bfd293a389c48afb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eb6fd82e5774079dc35",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d727de895797baab7684",
+    "url": "/static/js/2.568737a2.chunk.js"
+  },
+  {
+    "revision": "8eb6fd82e5774079dc35",
+    "url": "/static/js/main.3b2980f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
